9dcfffeaf8073302a77839994485beaf29b3b28e
Ludovic de Luna <ludovic.deluna@gmail.com>
2024-05-01 15:59:06 +0200

    Remove Thread version for Java fib

--- FibJava.java
+++ FibJava.java
@@ -4,60 +4,48 @@ import java.util.concurrent.Executors;
 
 class FibJava {
   public static void main(String[] _args) {
-    var runners = new FibRunnable[] {
-        new FibRunnable("worker 1"),
-        new FibRunnable("worker 2")
+    var runners = new Fibonacci[] {
+      new Fibonacci("worker 1"),
+      new Fibonacci("worker 2")
     };
 
-    run_all(runners);
-
-    for (var runner : runners) {
-      System.out.printf("Result %s: %d\n", runner.workerName, runner.get());
-    }
-
-    System.out.println("Program done. Exit.");
-  }
-
-  static void run_all(FibRunnable[] runners) {
     final var executor = Executors.newVirtualThreadPerTaskExecutor();
     try (executor) {
-      Arrays.stream(runners)
-          .map(runner -> executor.submit(runner))
-          .toList()
-          .forEach(future -> {
-            try {
-              future.get();
-            } catch (InterruptedException | ExecutionException _getException) {
-              System.out.println("Workers interrupted");
-              System.exit(1);
-            }
-          });
-      ;
-    } // block call shutdown() on executor when finished
+      var futures = Arrays.stream(runners).map(runner -> {
+        return executor.submit(() -> runner.run());
+      }).toList();
+
+      for (var future : futures) {
+        Fibonacci runner = future.get();
+        System.out.printf("Result %s: %d\n", runner.workerName, runner.result);
+      }
+    } catch (InterruptedException | ExecutionException _e) {
+      System.out.println("Workers interrupted");
+      System.exit(1);
+    } // call shutdown() on executor when finished
+    System.out.println("Program done. Exit.");
   }
 }
 
-class FibRunnable implements Runnable {
-  public final String workerName;
-  private long result = 0;
+class Fibonacci {
+  final String workerName;
+  public long result;
 
-  FibRunnable(String name) {
+  Fibonacci(String name) {
     this.workerName = name;
   }
 
-  @Override
-  public void run() {
+  Fibonacci run() {
     System.out.printf("%s started!\n", workerName);
-    this.result = FibRunnable.fib(50L);
-  }
-
-  public synchronized long get() {
-    return result;
+    result = Fibonacci.fib(50L);
+    return this;
   }
 
   static long fib(long n) {
-    if (n <= 1L)
+    if (n <= 1L) {
       return 1L;
+    }
+
     return fib(n - 1L) + fib(n - 2L);
   }
 }
